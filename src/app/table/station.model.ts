7200aa89c06b74fe9ff3c749bd1c7f4ea6e6465e
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2020-05-11 11:12:17 +0200

    model stacji poprawiony (camelCase na snake_case)

--- src/app/table/station.model.ts
+++ src/app/table/station.model.ts
@@ -2,13 +2,13 @@ export class Station {
   constructor(
     public idStacji: string,
     public stacja: string,
-    public dataPomiaru: string,
-    public godzinaPomiaru: string,
+    public data_pomiaru: string,
+    public godzina_pomiaru: string,
     public temperatura: string,
-    public predkoscWiatru: string,
-    public kierunekWiatru: string,
-    public wilgotnoscWzgledna: string,
-    public sumaOpadu: string,
+    public predkosc_wiatru: string,
+    public kierunek_wiatru: string,
+    public wilgotnosc_wzgledna: string,
+    public suma_opadu: string,
     public cisnienie: string,
   ) {
   }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -17,7 +17,6 @@ export class TableComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.onButtonClicked();
   }
 
   onButtonClicked() {
